9e31b3e8c5d66259d5ac8bff28be1de99c5db4e1
isaactwong <isaactwong@gmail.com>
2017-04-03 23:18:52 -0400

    Finished problem 4.

--- functors.hs
+++ functors.hs
@@ -22,7 +22,7 @@ Both are Functors of an ordered pair, but the first is only "functorial" in the 
 -}
 
 {-
-Implement a Functor instance for the type ITree, defined as
+3. Implement a Functor instance for the type ITree, defined as
 data ITree a = Leaf (Int -> a) 
              | Node [ITree a]
 -}
@@ -31,3 +31,16 @@ instance Functor ITree where
          fmap f (Leaf g)  = Lead (f . g)
          fmap f (Node ns) = Node (map (fmap f) ns)
          
+{-
+4. Give an example of a type of kind * -> * which cannot be made an instance of Functor (without using undefined).
+Help from http://stackoverflow.com/questions/7220436/good-examples-of-not-a-functor-functor-applicative-monad
+-}
+data T a = T (a -> Int)
+
+-- Can't write fmap. Given f :: a -> b, can't get T a -> T b
+
+{-
+5. Is this statement true or false? The composition of two Functors is also a Functor.
+If false, give a counterexample; if true, prove it by exhibiting some appropriate Haskell code. 
+-}
+-- True!
