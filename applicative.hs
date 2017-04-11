29390b6f534ec350839a18f4f14c974258c7b15b
isaactwong <isaactwong@gmail.com>
2017-04-10 22:51:12 -0400

    Implemented the function sequenceAL for sequences.

--- applicative.hs
+++ applicative.hs
@@ -1,3 +1,4 @@
+import Control.Applicative
 
 {-
 4.2.1
@@ -5,6 +6,8 @@
 One might imagine a variant of the interchange law that says something about applying a pure function to an effectful argument. Using the above laws, prove that
 pure f <*> x = pure (flip ($)) <*> x <*> pure f
 -}
+
+-- TODO
 
 class Functor f => MyApplicative f where
       pure' :: a -> f a
@@ -28,11 +31,21 @@ instance MyApplicative Maybe where
 
 -- 4.3.2 Determine the correct definition of pure for the ZipList instance of Applicative—
 -- there is only one implementation that satisfies the law relating pure and (<*>).
-newtype ZipList a = ZipList { getZipList :: [a] } deriving (Eq,Show)
+newtype ZippyList a = ZippyList { getZippyList :: [a] } deriving (Eq,Show)
 
-instance Functor ZipList where
-         fmap f (ZipList xs)= ZipList (fmap f xs)
+instance Functor ZippyList where
+         fmap f (ZippyList xs)= ZippyList (fmap f xs)
 
-instance MyApplicative ZipList where
-         pure' x = ZipList (repeat x)
-         (ZipList fs) <@> (ZipList xs) = ZipList (zipWith ($) fs xs)
+instance MyApplicative ZippyList where
+         pure' x = ZippyList (repeat x)
+         (ZippyList fs) <@> (ZippyList xs) = ZippyList (zipWith ($) fs xs)
+
+{-
+4.5.1 Implement a function sequenceAL :: Applicative f => [f a] -> f [a]
+
+There is a generalized version of this, sequenceA, which works for any Traversable (see the later section on Traversable), but implementing this version specialized to lists is a good exercise.
+-}
+
+sequenceAL :: Applicative f => [f a] -> f [a]
+sequenceAL [] = pure []
+sequenceAL (x:xs) = pure (:) <*> x <*> sequenceAL xs
