3e164875d3e7abda1747f2fd97a7bae78fd8fcae
isaactwong <isaactwong@gmail.com>
2017-04-26 23:31:20 -0400

    Cleaned up code and proved the first problem.

--- applicative.hs
+++ applicative.hs
@@ -5,36 +5,45 @@ import Control.Applicative
 
 One might imagine a variant of the interchange law that says something about applying a pure function to an effectful argument. Using the above laws, prove that
 pure f <*> x = pure (flip ($)) <*> x <*> pure f
+
+Hint from https://github.com/izubkov/Typeclassopedia/blob/master/Typeclassopedia.hs
 -}
 
--- TODO
+pure (flip ($)) <*> x <*> pure f                  =
+(pure (flip ($)) <*> x) <*> pure f                =
+pure ($ f) <*> (pure (flip ($)) <*> x)            =
+pure (.) <*> pure ($ f) <*> pure (flip ($)) <*> x = 
+pure ((.) ($ f)) <*> pure (flip ($)) <*> x        =
+pure ((.) ($ f) (flip ($))) <*> x                 =
+pure (($ f) . flip ($)) <*> x                     =
+pure f <*> x
+
+-- all this imples that
+pure (flip ($)) <*> x <*> pure f = pure f <*> x -- QED
 
 class Functor f => MyApplicative f where
       pure' :: a -> f a
-      --infix1 4 <@>, @>, <@
       (<@>) :: f (a -> b) -> f a -> f b
 
 {-
-      (@>) :: f a -> f b -> f b
-      a1 @> a2 = (id <$ a1) <@> a2
-
-      (<@) :: f a -> f b -> f a
-      (<@) = liftA2 const
+4.3.1 Define an applicative instance for Maybe
 -}
 
--- 4.3.1 Define an applicative instance for Maybe
 instance MyApplicative Maybe where
-         pure' x = Just x
-         Nothing <@> _ = Nothing
-         _ <@> Nothing = Nothing
+         pure' x           = Just x
+         Nothing <@> _     = Nothing
+         _ <@> Nothing     = Nothing
          Just f <@> Just x = Just (f x)
 
--- 4.3.2 Determine the correct definition of pure for the ZipList instance of Applicative—
--- there is only one implementation that satisfies the law relating pure and (<*>).
+{-
+4.3.2 Determine the correct definition of pure for the ZipList instance of Applicative—
+there is only one implementation that satisfies the law relating pure and (<*>).
+-}
+
 newtype ZippyList a = ZippyList { getZippyList :: [a] } deriving (Eq,Show)
 
 instance Functor ZippyList where
-         fmap f (ZippyList xs)= ZippyList (fmap f xs)
+         fmap f (ZippyList xs) = ZippyList (fmap f xs)
 
 instance MyApplicative ZippyList where
          pure' x = ZippyList (repeat x)
@@ -50,18 +59,24 @@ sequenceAL :: Applicative f => [f a] -> f [a]
 sequenceAL []     = pure []
 sequenceAL (x:xs) = pure (:) <*> x <*> sequenceAL xs
 
--- 4.6.1 Implement pure and (<*>) in terms of unit and (**), and vice versa.
+{-
+4.6.1 Implement pure and (<*>) in terms of unit and (**), and vice versa.
+-}
+
 class Functor f => Monoidal f where
       unit :: f ()
       (**) :: f a -> f b -> f (a,b)
 
 -- Implementations of pure and <*> in terms of unit and **
--- pure a    = fmap (const a) unit
--- ff <*> fa = fmap (uncurry $) (ff ** fa)
+pure a    = fmap (const a) unit
+ff <*> fa = fmap (uncurry $) (ff ** fa)
 
--- 4.6.2
--- Are there any Applicative instances for which there are also functions
--- f () -> () and f (a,b) -> (f a, f b), satisfying some "reasonable" laws?
+{-
+4.6.2
+Are there any Applicative instances for which there are also functions
+f () -> () and f (a,b) -> (f a, f b), satisfying some "reasonable" laws?
+-}
+
 instance Monoidal Maybe where
          unit                 = Just ()
          Nothing ** _         = Nothing
@@ -85,3 +100,4 @@ unjoin (Just (x,y)) = (Just x, Just y)
 -- Implentations of unit and ** in terms of pure and <*>
 -- unit = pure ()
 -- fa ** fb = pure (,) <*> fa <*> fb
+-- todo
