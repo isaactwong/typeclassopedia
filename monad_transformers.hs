12c208fc6b3878074d92a100a2882bc85b572844
isaactwong <isaactwong@gmail.com>
2017-05-10 22:32:12 -0400

    Finished the two monad transformer problems.

--- monad_transformers.hs
+++ monad_transformers.hs
@@ -7,3 +7,11 @@ class MonadTrans t where
 
 -- The kind of t is (*->*)->*->*
 -- Because t takes a Monad which is of kind (*->*) and a value which is of type (*) and returns a value (*).
+
+{-
+6.4.1 Implement join :: M (N (M (N a))) -> M (N a), given distrib :: N (M a) -> M (N a) and assuming M and N are instances of Monad.
+-}
+
+join :: M (N (M (N a))) -> M (N a)
+join m = join ((((fmap . fmap) join) (fmap distrib m)))
+-- TODO Holy cow, is this right? How can we simplify it?
