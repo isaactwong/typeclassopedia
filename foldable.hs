9ed6942370d26033e9b45416f51b67030c0c938d
isaactwong <isaactwong@gmail.com>
2017-05-11 23:03:31 -0400

    Calculated the types of foldMap compositions.

---
+++ foldable.hs
@@ -0,0 +1,8 @@
+{-
+11.2.1 What is the type of foldMap . foldMap? Or foldMap . foldMap . foldMap, etc.? What do they do?
+-}
+
+(foldMap . foldMap) :: (Foldable t, Foldable t2) => (a -> m) -> t (t2 a) -> b
+(foldMap . foldMap . foldMap) :: (Foldable t, Foldable t2, Foldable t3) => (a -> m) -> t (t2 (t3 a)) -> b
+
+-- Continually reduces nested structures to a single value.
