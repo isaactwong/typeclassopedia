c64af560fa785ab7c189fad227ae470f8261d2d9
isaactwong <isaactwong@gmail.com>
2017-05-14 22:51:15 -0400

    Implememted toList for the Foldable class.

--- foldable.hs
+++ foldable.hs
@@ -6,3 +6,10 @@
 (foldMap . foldMap . foldMap) :: (Foldable t, Foldable t2, Foldable t3) => (a -> m) -> t (t2 (t3 a)) -> b
 
 -- Continually reduces nested structures to a single value.
+
+{-
+11.3.1 Implement toList :: Foldable f => f a -> [a].
+-}
+
+toList :: (Foldable t) => f a -> [a]
+toList = foldmap (:[])
