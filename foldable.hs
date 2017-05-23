ce268ea044313899c468c008f404dc51793cec26
isaactwong <isaactwong@gmail.com>
2017-05-22 22:31:36 -0400

    Implemented any and all.

--- foldable.hs
+++ foldable.hs
@@ -21,8 +21,27 @@ toList = foldMap (:[])
 11.3.2 Pick some of the following functions to implement: concat, concatMap, and, or, any, all, sum, product, maximum(By), minimum(By), elem, notElem, and find. Figure out how they generalize to Foldable and come up with elegant implementations using fold or foldMap along with appropriate Monoid instances.
 -}
 
-concat_ :: (Foldable t) => t [a] -> [a]
-concat_ xs = foldMap id xs
+concat :: (Foldable t) => t [a] -> [a]
+concat xs = foldMap id xs
 
 concatMap :: (Foldable t) => (a -> [b]) -> t a -> [b]
 concatMap f xs = foldMap f xs
+
+newtype All = All { getAll :: Bool } deriving (Eq, Show, Ord, Read)
+instance Monoid All where
+         mempty                  = All True
+         mappend (All x) (All y) = All (x && y)
+
+and :: (Foldable t) => t Bool -> Bool
+and = getAll . foldMap All 
+
+newtype Any = Any { getAny :: Bool } deriving (Eq, Show, Ord, Read)
+instance Monoid Any where
+         mempty               = Any False
+         mappend (Any True) _ = Any True
+         mappend _ (Any True) = Any True
+         mappend _ _          = Any False
+
+any :: (Foldable t) => t Bool -> Bool
+any = getAny . foldMap Any
+         
