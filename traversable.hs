d416d6dce1bd6f6bd714c0950eebb10f47098b02
isaactwong <isaactwong@gmail.com>
2017-06-21 23:10:46 -0400

    Implemented Tree as Traversable and then two examples of tree of lists to lists of trees.

--- traversable.hs
+++ traversable.hs
@@ -1,11 +1,33 @@
-data Tree a = Empty | Leaf a | Node (Tree a) a (Tree a)
+data Tree a = Empty | Leaf a | Node (Tree a) a (Tree a) deriving (Show)
+t1 = Node (Leaf [2,3]) [1] (Node (Leaf [6]) [4,5] (Leaf [7,8]))
+t2 = Node (Leaf (Just 2)) (Just 4) (Node (Leaf (Just 5)) (Just 7) (Leaf (Just 8)))
+t3 = Node (Leaf (Just 2)) (Just 4) (Node (Leaf (Just 5)) (Just 7) (Leaf (Nothing)))
+
+instance Functor Tree where
+  fmap f Empty               = Empty
+  fmap f (Leaf a)            = Leaf (f a)
+  fmap f (Node left a right) = Node (fmap f left) (f a) (fmap f right)
+
+instance Foldable Tree where
+  foldMap f Empty               = mempty
+  foldMap f (Leaf a)            = f a
+  foldMap f (Node left a right) = (foldMap f left) `mappend` (f a) `mappend` (foldMap f right)
+  
+  
+instance Traversable Tree where
+  sequenceA Empty               = pure Empty
+  sequenceA (Leaf a)            = Leaf <$> a
+  sequenceA (Node left a right) = Node <$> (sequenceA left) <*> a <*> (sequenceA right)
 
 {-
 13.2.1 There are at least two natural ways to turn a tree of lists into a list of trees. What are they, and why?
 -}
 
--- example1 :: Tree ([a]) -> [Tree a]
--- example2 :: Tree ([a]) -> [Tree a]
+example1 :: Tree ([a]) -> [Tree a]
+example1 = sequenceA
+
+example2 :: Tree ([a]) -> [Tree a]
+example2 = foldMap (fmap Leaf)
 
 {-
 13.2.2 Give a natural way to turn a list of trees into a tree of lists.
